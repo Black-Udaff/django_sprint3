28207586789f290cfaffd5c6d02ea8106700798b
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-08-15 16:13:07 +0300

    123

---
+++ blogicum/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from blog.models import Post
+
+
+
+
+def index(request):
+    template = "blog/index.html"
+    post_list = Post.objects.values()
+    context = {"posts": reversed(posts)}
+    return render(request, template, context)
+
+
+def post_detail(request, pk):
+    template = "blog/detail.html"
+    for post in posts:
+        if post['id'] == pk:
+            context = {'post': post}
+            return render(request, template, context)
+    return HttpResponseNotFound("Пост не найден")
+
+
+def category_posts(request, category_slug):
+    template = "blog/category.html"
+    context = {"category_slug": category_slug}
+    return render(request, template, context)
